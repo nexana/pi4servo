b94174e89f8f374bb909f2688576c1d5b9c606e3
nexana <toon@nexana.be>
2020-07-08 11:32:41 +0200

    ad build + change value

--- src/main/java/be/nexana/pi4servo/controller/ServoController.java
+++ src/main/java/be/nexana/pi4servo/controller/ServoController.java
@@ -1,6 +1,7 @@
 package be.nexana.pi4servo.controller;
 
 import com.pi4j.io.gpio.*;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,24 +9,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class ServoController {
 
     private static GpioPinPwmOutput pin = null;
+    private static GpioController gpio = null;
 
     @RequestMapping("/")
     public String greeting() {
         return "hi";
     }
 
-    @RequestMapping("/open")
-    public String open() throws InterruptedException {
+    @RequestMapping("/open/{pwmValue}")
+    public String open(@PathVariable String pwmValue) throws InterruptedException {
         if (pin == null) {
-            GpioController gpio = GpioFactory.getInstance();
+            gpio = GpioFactory.getInstance();
             pin = gpio.provisionPwmOutputPin(RaspiPin.GPIO_24, "pulsePin");
 
         }
-//long netraulPulse = TimeUnit.MICROSECONDS.
-        pin.setPwm(800);
+        pin.setMode(PinMode.PWM_OUTPUT);
+        pin.setPwmRange(1000);
+        pin.setPwm(Integer.parseInt(pwmValue));
 
-        Thread.sleep(2500);
-        pin.setPwm(0);
+        gpio.shutdown();
+
         return "ok done";
     }
 
